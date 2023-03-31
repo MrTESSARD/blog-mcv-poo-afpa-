3657bd16c00a1b40b63bf5c0faad0762c51048ed
MrTESSARD <dimitri@tessard.pro>
2023-03-31 16:27:46 +0200

    maj

--- config/dev.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-const DB_HOST = "localhost";
-const DB_NAME = "blog-mvc-poo";
-const DB_USER = "root";
-const DB_PASSWORD = "root";
-
-const DB_FULL_HOST = "mysql:host=localhost;dbname=blog-mvc-poo";
--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../config/dev.php";
-require_once "../config/Autoloader.php";
-use App\config\Autoloader;
-Autoloader::register();
-
-use App\config\Router;
-$router = new Router();
-$router->run();
